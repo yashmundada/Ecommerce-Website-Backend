c4282e9f3e87b5f80194f8bd60fb100cdd760483
Yash Mundada <yash.mundada143@gmail.com>
2025-01-05 23:50:26 +0530

    AddNewProduct is created.

---
+++ src/main/java/com/ecommerce/productservicenovermber2024/Exception/ErrorResponse.java
@@ -0,0 +1,14 @@
+package com.ecommerce.productservicenovermber2024.Exception;
+
+import lombok.*;
+import org.springframework.http.HttpStatus;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ErrorResponse {
+    private String message;
+    private boolean success;
+    private HttpStatus status;
+}
